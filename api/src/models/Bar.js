bd259151959b150dff4d879451901d12a08ad83b
Gasnis <gastonsaravia112@gmail.com>
2023-01-14 19:23:17 +0100

    Bar

--- api/src/models/Bar.js
+++ api/src/models/Bar.js
@@ -3,16 +3,15 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define('Bar', {
     id:{
       type: DataTypes.NUMBER,
-
     },
     name: {
       type: DataTypes.NUMBER,
       allowNull: false,
     },
-    flag: {
+    categorie: {
       type: DataTypes.STRING,
     },
   });
