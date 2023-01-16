a53b83e409c4d15fac4309913f3eb15bfde8e407
ThomRojas <thomas.rojas.arce@gmail.com>
2023-01-16 20:02:45 -0300

    create(api/src/models/Local): Se creó el modelo de Local

--- api/src/models/Local.js
+++ api/src/models/Local.js
@@ -4,22 +4,72 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('local', {
-    id:{
-      type: DataTypes.INTEGER,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.UUID, // email
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+      primaryKey: true,
       allowNull: false,
     },
-    categorie: {
+    // password: {
+    //   type: Datatypes.STRING,
+    //   allowNull:false,
+    // },
+    name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
-    tables: {
+    category: {
+      type: DataTypes.ENUM,
+      values: ["disco", "pub", "bar"],
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.TEXT,// string 255 / text unlimited
+      allowNull: true, 
+    },
+    location: {
       type: DataTypes.STRING,
+      allowNull: true, // **
+    },
+    menu: {
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     event: {
       type: DataTypes.BOOLEAN,
-    }
+      allowNull: true,
+    },
+    capacity: {
+      type: DataTypes.INTEGER,/* A data type. */
+      allowNull: false,
+    },
+    petFriendly: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    ageRange: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER), // Para colocar "18+" "18  - 40" "18++"
+      allowNull: true,
+    },
+    phone: {
+      type: DataTypes.STRING, //"+4542545875423"
+      allowNull: true,
+    },
+    promo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    bookPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    rating: {
+        type: DataTypes.INTEGER, //  || integer
+      },
   });
 };
