56deadc3233127897e3a636e58d418b516b31b05
Nelson Giovanny Vasquez Blanco <gioh2020@gmail.com>
2023-01-16 21:14:46 -0500

    create(api/src/controller/user.controller.js): se creo el controlador del archivo user.route.js, este controlador es el que va a tener toda la logica

--- api/src/routes/userRoutes/user.route.js
+++ api/src/routes/userRoutes/user.route.js
@@ -1,4 +1,5 @@
-const {Router} =require("express")
+const {Router} = require("express")
+const postUserData = require("../../controllers/user.controller")
 
 
 const router = Router()
@@ -7,7 +8,7 @@ router.post("/", async(req, res)=>{
     try {
         const userData = req.body;
         const userDataCreated = await postUserData(userData)
-        res.status(200).jason(userDataCreated)
+        res.status(200).json(userDataCreated)
     } catch (error) {
         res.status(400).send(error.message)
     }
