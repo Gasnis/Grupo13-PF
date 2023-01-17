ccc37ed81f2d3c53f9ee4d93cf812a1377848b51
sergiozampieri <gersio.zampieri@gmail.com>
2023-01-17 11:52:10 -0300

    redireccione el archivo local.route/ruta get:id

--- api/src/routes/userRoutes/local.route.js
+++ api/src/routes/userRoutes/local.route.js
@@ -1,4 +1,5 @@
 const {Router} = require("express")
+const { Local, User, Book } = require("../../db");
 const postLocalData = require("../../controllers/localControllers/local.controller")
 
 
@@ -15,3 +16,28 @@ router.post("/", async(req, res)=>{
 })
 
 module.exports = router;
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      if (id) {
+        const local = await Local.findAll({
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          }
+        });
+        const localById = local.filter((l) =>
+          l.id.includes(id)
+        );
+        if (localById.length === 0) {
+          {
+            return res.status(404).send("Local not found");
+          }
+        }
+        return res.status(200).send(localById);
+      }
+    } catch (error) {
+      return res.status(400).send("Local could not load properly");
+    }
+  });
