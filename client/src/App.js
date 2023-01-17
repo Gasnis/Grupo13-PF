7f1848bb1bf4a9bf466691eae4529794a24daf33
Sol <sol.gomez@mi.unc.edu.ar>
2023-01-17 14:40:28 -0800

    add (client/App.js): agregue rutas login, bookings y corregi newplace

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { Route } from "react-router-dom";
 import Home from './components/Home/Home.jsx'
 import Detail from './components/Details/Details.jsx'
-import Form from './components/FormBook/FormBook.jsx';
+import FormBook from './components/FormBook/FormBook.jsx';
+import FormBar from './components/FormBar/FormBar.jsx';
+import Login from './components/Login/Login.jsx';
 // import axios from "axios";
 // axios.defaults.baseURL = 
 axios.defaults.baseURL = "http://localhost:3001";
@@ -15,9 +17,13 @@ function App() {
 
         <Route path="/place/:id"> <Detail/> </Route>
 
-        <Route path="/newplace"> <Form/> </Route>
+        <Route path="/newplace"> <FormBar/> </Route>
         
         <Route path="/detail"> <Detail/> </Route>
+
+        <Route path="/login"> <Login/> </Route>
+
+        <Route path="/bookings"> <FormBook/> </Route>
 
       </div>
     
