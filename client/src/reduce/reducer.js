5ed6736013c7ce1bedbf280666c26741ce39c1b2
Sebastián Flórez <sebas.fj@hotmail.com>
2023-01-16 20:06:22 -0500

    ok estructura basica index - store - reducer

---
+++ client/src/reduce/reducer.js
@@ -0,0 +1,16 @@
+import {
+    //importar aquí las actions
+} from "./actions"
+
+const initialState = {
+    
+}
+
+export default function reducer (state = initialState, action) {
+    switch(action.type){
+        
+
+        default:
+            return state;
+    }
+}
---
+++ client/src/reduce/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+   reducer,
+   composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
