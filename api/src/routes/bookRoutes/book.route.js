5b521319b15b07a55f6c5ddca15ef0bf47108354
Josecanelo <97995082+Josecanelo@users.noreply.github.com>
2023-01-18 15:56:53 -0300

    Update book.route.js

--- api/src/routes/bookRoutes/book.route.js
+++ api/src/routes/bookRoutes/book.route.js
@@ -15,21 +15,21 @@ router.post("/", async (req, res) => {
     } catch (error) {
       res.status(404).send(error.message);
     }
-  });
+});
 
-  router.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
   
-      try {
-        if (id) {
-          const bookById = await getBookDetail(id)
-          return res.status(200).send(bookById);
-        }
-      } catch (error) {
-        return res.status(400).send(error.message);
+
+    try {
+      if (id) {
+        const bookById = await getBookDetail(id)
+        return res.status(200).send(bookById);
       }
-    });
+    } catch (error) {
+      return res.status(400).send(error.message);
+    }
+});
 
 
 
