699183cd9f2dfc2331c586e71b798653fd73fccc
Sol <sol.gomez@mi.unc.edu.ar>
2023-01-17 19:03:45 -0800

    add (components/signUp y Login): los inputs ya cambiar en tiempo real

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Navbar from '../Navbar/Navbar';
 
 export default function Login() {
+
+    const dispatch = useDispatch();
+    const [login, setLogin] = useState({
+        mail: "",
+        password: ""
+    })
     
+    function handleChange(event) {
+        setLogin({
+            ...login,
+            [event.target.name]: [event.target.value]
+        })
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-
+        //que es lo que tengo que hacer aca? porque no hace un post 
+        //cambia una propiedad de algun estado?
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-             <input
-                 type='text' 
-                 placeholder='Mail'
-            />
-            <input
-                 type='text' 
-                 placeholder='Contraseña'
-            />
-            <button>Ingresar</button>
-            <h3>Ingresar con Google</h3>
-            <Link to="/sign-up" >Todavia no tenes una cuenta?</Link>
-        </form>
+        <div>
+            <Navbar/>
+            <h1>Ingresa</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text' 
+                    placeholder='Mail'
+                    value={login.mail}
+                    name="mail"
+                    onChange={handleChange}
+                />
+                <input
+                    type='password' 
+                    placeholder='Contraseña'
+                    value={login.password}
+                    name="password"
+                    onChange={handleChange}
+                />
+                <button type="submit" id="loginButton">Ingresar</button>
+                <Link to="/sign-up" >Todavia no tenes una cuenta?</Link>
+            </form>
+        </div>
     )
 }
--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,38 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Navbar from '../Navbar/Navbar';
 
 export default function SignUp() {
 
+    const dispatch = useDispatch();
+
+    const [signUp, setSignUp] = useState({
+        name: "",
+        mail: "",
+        password: "",
+        phoneNumber: "",
+        birthday: "",
+    })
+
+    function handleChange(event) {
+        setSignUp({
+            ...signUp,
+            [event.target.name]: [event.target.value]
+        })
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
+        //despachar action q postee el user
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-        <input 
-            type='text' 
-            placeholder='Nombre'
-        /> 
-        <input
-             type='text' 
-             placeholder='Mail'
-        />
-        <input
-             type='text' 
-             placeholder='Contraseña'
-        />
-        <input
-             type='text' 
-             placeholder='Teléfono'
-        />
-        <input
-             type='date' 
-             placeholder='Fecha de cumpleaños'
-        />
-        <button>Registrarse</button>
-        <h3>Ingresar con Google</h3>
-        <Link to="/login" >Ya tenes una cuenta?</Link>
-        
-    </form>
+        <div>
+            <Navbar/>
+            <h1>Registrate</h1>
+            <form onSubmit={handleSubmit}>
+            <input 
+                type='text' 
+                placeholder='Nombre'
+                value={signUp.name}
+                name="name"
+                onChange={handleChange}
+            /> 
+            <input
+                type='text' 
+                placeholder='Mail'
+                value={signUp.mail}
+                name="mail"
+                onChange={handleChange}
+            />
+            <input
+                type='text' 
+                placeholder='Contraseña'
+                value={signUp.password}
+                name="password"
+                onChange={handleChange}
+            />
+            <input
+                type='text' 
+                placeholder='Teléfono'
+                value={signUp.phoneNumber}
+                name="phoneNumber"
+                onChange={handleChange}
+            />
+            <input
+                type='date' 
+                placeholder='Fecha de cumpleaños'
+                value={signUp.birthday}
+                name="birthday"
+                onChange={handleChange}
+            />
+            <h3>Sos dueño de un bar?</h3>
+            <button type="submit" id="signUpButton">Registrarse</button>
+            <h4>Ingresar con Google</h4>
+            <Link to="/login" >Ya tenes una cuenta?</Link>
+            
+        </form>
+
+        </div>
     )
 }
