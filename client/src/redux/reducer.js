9b08c319faf582aa8c917361d6d41368be465ba6
Gasnis <gastonsaravia112@gmail.com>
2023-01-20 18:28:20 +0100

    filtors y detail

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,7 +3,7 @@ import {
     SEARCH_PLACE,
     GET_PLACE_DETAIL,
     GET_PLACES,
-    FILTER_PLACES,
+    FILTER_CATEGORY,
     GET_USER,
     GET_USER_BY_ID,
     LOGOUT
@@ -33,10 +33,28 @@ export default function reducer (state = initialState, action) {
                 ...state,
                 places: action.payload,
             }
-        case FILTER_PLACES:
+
+        case FILTER_CATEGORY:
+            // let filterCategory = []
+            let lugaresfiltrados = state.allPlaces.filter(lugar => action.payload.includes(lugar.category))
+
+            // action.paylodoad.map(category => category === state.allPlaces.category)
+            // if(action.payload === 'bar'){
+            //     let filter = state.allPlaces.filter(place =>place.category === "bar")
+            //     filterCategory.push(filter)
+            // }
+            // if(action.payload === "pub"){
+            //     let filter =  state.allPlaces.filter( place => place.category === "pub")
+            //     filterCategory.push(filter)
+            // }
+            // if(action.payload === 'disco'){
+            //     let filter = state.allPlaces.filter(place => place.category === "disco")
+            //     filterCategory.push(filter)
+
+            
             return {
                 ...state,
-                places: action.payload,
+                places: lugaresfiltrados 
             }
 
         case GET_PLACE_DETAIL:
