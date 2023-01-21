64eff2a50bee517788932a82f51c9090f903067f
sergiozampieri <gersio.zampieri@gmail.com>
2023-01-21 11:37:26 -0300

    los filtros ahora funcionan, pero estan feos

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -57,7 +57,7 @@ export default function Navbar(props) {
                             
                         </Link>
 
-                        <Filtros/>
+                        
 
                     </div>
                     : null}
