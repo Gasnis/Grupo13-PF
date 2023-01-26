27f4d277ddf7ef3b1954b856a0938d853b84ea15
sergiozampieri <gersio.zampieri@gmail.com>
2023-01-26 09:07:09 -0300

    se integro redux-persist al store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
 import rootReducer from './reducer';
 import {applyMiddleware, createStore, compose} from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+let store = createStore(persistedReducer, composeEnhancer(applyMiddleware(thunk)))
 
-export default store;
+let persistor = persistStore(store)
+
+export {store,persistor}
+
+
+  
