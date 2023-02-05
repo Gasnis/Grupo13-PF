7bdd0d39e7675954303cca0e80c57f808fe8361a
Sol <sol.gomez@mi.unc.edu.ar>
2023-02-05 13:32:40 -0800

    about terminado + transiciones de dark mode

--- client/src/components/About Us/AboutUs.jsx
+++ client/src/components/About Us/AboutUs.jsx
@@ -7,7 +7,7 @@ import Seba from "../../utils/AboutUs/Seba.jpg";
 import Gio from "../../utils/AboutUs/Gio.jpg";
 import Jose from "../../utils/AboutUs/jose.jpeg";
 import Sol from "../../utils/AboutUs/Sol.jpg";
-import wwwhere from "../../utils/wwwhere.png";
+import Thomas from "../../utils/AboutUs/Thomas.png"
 import styles from "../About Us/AboutUs.module.css";
 import { useSelector } from "react-redux";
 
@@ -25,7 +25,7 @@ export default function AboutUs() {
     {
         name: "Ignacio Luna",
         image: Igna,
-        linkedin: "",
+        linkedin: "https://www.linkedin.com/in/ignaluna/",
         github: "https://github.com/ignaluna",
     },
     {
@@ -43,13 +43,13 @@ export default function AboutUs() {
     {
         name: "Jose Canelo",
         image: Jose,
-        linkedin: "",
+        linkedin: "https://www.linkedin.com/in/jose-canelo-suarez-06ba71223/",
         github: "https://github.com/Josecanelo",
     },
     {
         name: "Thomas Rojas",
-        image: wwwhere,
-        linkedin: "https://www.linkedin.com/in/jose-canelo-suarez-06ba71223/",
+        image: Thomas,
+        linkedin: "https://www.linkedin.com/in/thomas-rojas-719a1a258",
         github: "https://github.com/ThomRojas",
     },
     {
@@ -61,7 +61,7 @@ export default function AboutUs() {
     {
         name: "Sebastian Florez ",
         image: Seba,
-        linkedin: "",
+        linkedin: "https://www.linkedin.com/in/sebasfj",
         github: "https://github.com/SebasFj",
     },
     ]
@@ -80,10 +80,10 @@ export default function AboutUs() {
                             <h4>FullStack Developer </h4>
                             <div className={styles.linksContainer}>
                                 <a href={dev.github} >
-                                    <svg className={styles.gitHub} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+                                    <svg className={styles.gitHub} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
                                 </a>
                                 <a href={dev.linkedin}>
-                                    <svg className={styles.linkedin} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" /></svg>
+                                    <svg className={styles.linkedin} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" /></svg>
                                 </a>
                             </div>
                         </div>
