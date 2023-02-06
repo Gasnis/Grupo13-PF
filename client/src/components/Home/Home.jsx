f5dad45e237ecb62135a10041daebc89672f5d84
Sol <sol.gomez@mi.unc.edu.ar>
2023-02-05 17:55:05 -0800

    Agregamos carrusel de promos y cambios css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -8,6 +8,7 @@ import {
     getPlaces
 } from "../../redux/actions";
 import Loading from "../Loading/Loading";
+import Promos from "../Carousel/Carousel";
 
 export default function Home() {
 
@@ -36,15 +37,16 @@ export default function Home() {
     }
 
     return (
-        <div className={StyleSheet.principalDiv}>
+        <div className={style.principalDiv}>
 
             <Navbar home={true} />
-
-
+            <Promos />
             <div className={darkmode ? style.info : style.infodark}>
-
-                <div>
-                    <input className={darkmode ? style.searchbar : style.searchbardark} onChange={handleSearchBar} type="search" placeholder="Busca tu bar" />
+                <div className={style.centrarSearch}>
+                    <div className={style.searchBarContainer}>
+                        <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xNS44NTMgMTYuNTZjLTEuNjgzIDEuNTE3LTMuOTExIDIuNDQtNi4zNTMgMi40NC01LjI0MyAwLTkuNS00LjI1Ny05LjUtOS41czQuMjU3LTkuNSA5LjUtOS41IDkuNSA0LjI1NyA5LjUgOS41YzAgMi40NDItLjkyMyA0LjY3LTIuNDQgNi4zNTNsNy40NCA3LjQ0LS43MDcuNzA3LTcuNDQtNy40NHptLTYuMzUzLTE1LjU2YzQuNjkxIDAgOC41IDMuODA5IDguNSA4LjVzLTMuODA5IDguNS04LjUgOC41LTguNS0zLjgwOS04LjUtOC41IDMuODA5LTguNSA4LjUtOC41eiIvPjwvc3ZnPg==" />
+                        <input className={darkmode ? style.searchbar : style.searchbardark} onChange={handleSearchBar} type="search" placeholder="Busca tu bar..." />
+                    </div>
                 </div>
                 <div>
                     <div className={darkmode ? style.info : style.infodark}>
